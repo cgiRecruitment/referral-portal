2688cdeeab63534283a83f54b0c96ad267f37f50
CGIBIFE <arul.natarajan@cgi.com>
2019-03-21 20:22:14 +0100

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import NavBar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+
+class Header extends React.Component{
+
+    render() {
+        return(
+            <NavBar bg="dark" variant="dark" expand="lg">
+                <NavBar.Brand href="#home">Referral portal</NavBar.Brand>
+                <NavBar.Toggle aria-controls="basic-navbar-nav" />
+                <NavBar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Dashboard</Nav.Link>
+                        <Nav.Link href="#link">View Profiles</Nav.Link>
+                        <Nav.Link href="#link">Add Profiles</Nav.Link>
+                    </Nav>
+                </NavBar.Collapse>
+            </NavBar>
+        )
+    }
+
+}
+
+export default Header

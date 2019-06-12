b9e730df1cb3e9e1607f15744f21956d94544c31
nikhila.ghai <nikhila.ghai@cgi.com>
2019-06-12 10:43:05 +0200

    cosmetic changes

--- src/components/Status.js
+++ src/components/Status.js
@@ -5,9 +5,13 @@ class Status extends React.Component {
   render() {
     return (
       <span
-        className={`status ${ this.props.children!=null && (this.props.children.includes("Rejected") || this.props.children.includes("decline")) &&
-          "rejected"} ${this.props.children!=null && this.props.children.includes("Joiner") &&
-          "accepted"}`}
+        className={`status ${this.props.children != null && (this.props.children.includes("Rejected") || this.props.children.includes("decline")) &&
+          "rejected"} ${this.props.children != null && this.props.children.includes("Joined") &&
+          "accepted"} ${this.props.children != null && this.props.children.includes("Application Received") &&
+          "received"} ${this.props.children != null && this.props.children.includes("Offer Made") &&
+          "offer"} ${this.props.children != null && this.props.children.includes("On hold") &&
+          "hold"} ${this.props.children != null && this.props.children.includes("Offer Declined") &&
+          "declined"}`}
       >
         {this.props.children}
       </span>
